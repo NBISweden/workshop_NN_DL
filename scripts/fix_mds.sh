625b05d4481e4a8bf87b3bd978d4bb51e8020a0e
clami66 <claudio.mirabello@scilifelab.se>
2026-04-30 13:29:42 +0200

    limit changes to lectures

--- scripts/fix_mds.sh
+++ scripts/fix_mds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for md in $(find . -name "*.md"); do
+for md in $(find basics/ good_practices/ -name "*.md"); do
     echo "Removing cell_style and slideshow tags from $md..."
     sed -i 's/```python.*/```python/g' $md
 done
